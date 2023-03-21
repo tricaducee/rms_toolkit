eed30209898d629ef1c96344590894124aa4279c
Hermès Rolle <tricaducee@gmail.com>
2023-03-21 01:58:16 +0100

    improve 960

--- 960/main.c
+++ 960/main.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#define KING 0
+#define QUEEN 1
+#define ROOK 2
+#define BISHOP 3
+#define KNIGHT 4
+#define PAWN 5
+#define UNI_KING 6
+#define UNI_QUEEN 7
+#define UNI_ROOK 8
+#define UNI_BISHOP 9
+#define UNI_KNIGHT 10
+#define UNI_PAWN 11
+#define N_PIECES 12
+#define NAME_MAX_SIZE 25
 
 void put_bishop(const char **position, const char *bishop, int first_place)
 {
@@ -43,27 +57,76 @@ void put_rook_and_king(const char **position, const char *rook, const char *king
 	position[i] = rook;
 }
 
-void print_position(const char **position)
+void generate_random_positions(const char **position, const char pieces[N_PIECES][NAME_MAX_SIZE])
 {
-	printf("| %s | %s | %s | %s | %s | %s | %s | %s |\n", position[0], position[1], position[2], position[3], position[4], position[5], position[6], position[7]);
+	put_bishop(position, pieces[BISHOP], 0);
+	put_bishop(position, pieces[BISHOP], 1);
+	put_knight_and_queen(position, pieces[KNIGHT]);
+	put_knight_and_queen(position, pieces[KNIGHT]);
+	put_knight_and_queen(position, pieces[QUEEN]);
+	put_rook_and_king(position, pieces[ROOK], pieces[KING]);
 }
 
-void print_pawn(const char *pawn)
+void generate_random_positions_uni(const char **position, const char pieces[N_PIECES][NAME_MAX_SIZE])
 {
-	printf("| %s | %s | %s | %s | %s | %s | %s | %s |\n", pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn);
+	put_bishop(position, pieces[UNI_BISHOP], 0);
+	put_bishop(position, pieces[UNI_BISHOP], 1);
+	put_knight_and_queen(position, pieces[UNI_KNIGHT]);
+	put_knight_and_queen(position, pieces[UNI_KNIGHT]);
+	put_knight_and_queen(position, pieces[UNI_QUEEN]);
+	put_rook_and_king(position, pieces[UNI_ROOK], pieces[UNI_KING]);
+}
+
+void print_position(const char **position, int line)
+{
+	printf(" %d | %s | %s | %s | %s | %s | %s | %s | %s | %d\n", line, position[0], position[1], position[2], position[3], position[4], position[5], position[6], position[7], line);
+}
+
+void print_pawn(const char *pawn, int line)
+{
+	printf(" %d | %s | %s | %s | %s | %s | %s | %s | %s | %d\n", line, pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, line);
 }
 
 void print_line(void)
 {
-	printf(" --- --- --- --- --- --- --- --- \n");
+	printf("    --- --- --- --- --- --- --- --- \n");
+}
+
+void print_legende(const char pieces[N_PIECES][NAME_MAX_SIZE])
+{
+	printf("\n    ------   ------ \n   | %s = %s | %s = %s |\n    ------   ------ \n   | %s = %s | %s = %s |\n    ------   ------ \n   | %s = %s | %s = %s |\n    ------   ------ \n\n", pieces[KING], pieces[UNI_KING], pieces[QUEEN], pieces[UNI_QUEEN], pieces[ROOK], pieces[UNI_ROOK], pieces[BISHOP], pieces[UNI_BISHOP], pieces[KNIGHT], pieces[UNI_KNIGHT], pieces[PAWN], pieces[UNI_PAWN]);
+}
+
+void print_chess(const char **position, const char pieces[N_PIECES][NAME_MAX_SIZE], const char *actual_pawn)
+{
+	print_legende(pieces);
+	printf("     a   b   c   d   e   f   g   h\n");
+	print_line();
+	print_position(position, 8);
+	print_line();
+	print_pawn(actual_pawn, 7);
+	print_line();
+	printf(" 6 |   | * |   | * |   | * |   | * | 6\n");
+	print_line();
+	printf(" 5 | * |   | * |   | * |   | * |   | 5\n");
+	print_line();
+	printf(" 4 |   | * |   | * |   | * |   | * | 4\n");
+	print_line();
+	printf(" 3 | * |   | * |   | * |   | * |   | 3\n");
+	print_line();
+	print_pawn(actual_pawn, 2);
+	print_line();
+	print_position(position, 1);
+	print_line();
+	printf("     a   b   c   d   e   f   g   h\n\n");
 }
 
 int return_error(const char *message, const char *arg)
 {
 	if (!arg)
-		printf("\033[31;1mERROR : !!! %s !!!\033[0m", message);
+		printf("\033[31;1mERROR : !!! %s !!!\033[0m\n", message);
 	else
-		printf("\033[31;1mERROR : !!! %s : %s !!!\033[0m", message, arg);
+		printf("\033[31;1mERROR : !!! %s : %s !!!\033[0m\n", message, arg);
 	return (1);
 }
 
@@ -73,66 +136,40 @@ int	main(int ac, char **av)
 		return (return_error("Too many arguments", NULL));
 	srand(time(NULL));
 	const char *position[8] = {NULL};
-	const char king[] = "\033[31;1mK\033[0m";
-	const char queen[] = "\033[35;1mQ\033[0m";
-	const char rook[] = "\033[33;1mR\033[0m";
-	const char bishop[] = "\033[34;1mB\033[0m";
-	const char knight[] = "\033[32;1mN\033[0m";
-	const char pawn[] = "\033[36;1mp\033[0m";
-
-	const char uni_king[] = "\033[31;1m♔\033[0m";
-	const char uni_queen[] = "\033[35;1m♕\033[0m";
-	const char uni_rook[] = "\033[33;1m♖\033[0m";
-	const char uni_bishop[] = "\033[34;1m♗\033[0m";
-	const char uni_knight[] = "\033[32;1m♘\033[0m";
-	const char uni_pawn[] = "\033[36;1m♙\033[0m";
+	const char pieces[N_PIECES][NAME_MAX_SIZE] = {
+		"\033[31;1mK\033[0m",
+		"\033[35;1mQ\033[0m",
+		"\033[33;1mR\033[0m",
+		"\033[34;1mB\033[0m",
+		"\033[32;1mN\033[0m",
+		"\033[36;1mp\033[0m",
+		"\033[31;1m♔\033[0m",
+		"\033[35;1m♕\033[0m",
+		"\033[33;1m♖\033[0m",
+		"\033[34;1m♗\033[0m",
+		"\033[32;1m♘\033[0m",
+		"\033[36;1m♙\033[0m"
+	};
 
 	const char *actual_pawn;
 
 	if (ac == 2)
 	{
-		if (!strcmp(av[1], "uni") && !strcmp(av[1], "--uni") && !strcmp(av[1], "-u"))
-			return (return_error("Invalid argument", av[1]));
-		else
+		if (!strcmp(av[1], "uni") || !strcmp(av[1], "--uni") || !strcmp(av[1], "-u"))
 		{
-			put_bishop(position, uni_bishop, 0);
-			put_bishop(position, uni_bishop, 1);
-			put_knight_and_queen(position, uni_knight);
-			put_knight_and_queen(position, uni_knight);
-			put_knight_and_queen(position, uni_queen);
-			put_rook_and_king(position, uni_rook, uni_king);
-			actual_pawn = uni_pawn;
+			generate_random_positions_uni(position, pieces);
+			actual_pawn = pieces[UNI_PAWN];
 		}
+		else
+			return (return_error("Invalid argument", av[1]));
 	}
 	else
 	{
-		put_bishop(position, bishop, 0);
-		put_bishop(position, bishop, 1);
-		put_knight_and_queen(position, knight);
-		put_knight_and_queen(position, knight);
-		put_knight_and_queen(position, queen);
-		put_rook_and_king(position, rook, king);
-		actual_pawn = pawn;
+		generate_random_positions(position, pieces);
+		actual_pawn = pieces[PAWN];
 	}
 
-	printf("\n ------   ------ \n| %s = %s | %s = %s |\n ------   ------ \n| %s = %s | %s = %s |\n ------   ------ \n| %s = %s | %s = %s |\n ------   ------ \n\n\n", king, uni_king, queen, uni_queen, rook, uni_rook, bishop, uni_bishop, knight, uni_knight, pawn, uni_pawn);
-	print_line();
-	print_position(position);
-	print_line();
-	print_pawn(actual_pawn);
-	print_line();
-	printf("|   | * |   | * |   | * |   | * |\n");
-	print_line();
-	printf("| * |   | * |   | * |   | * |   |\n");
-	print_line();
-	printf("|   | * |   | * |   | * |   | * |\n");
-	print_line();
-	printf("| * |   | * |   | * |   | * |   |\n");
-	print_line();
-	print_pawn(actual_pawn);
-	print_line();
-	print_position(position);
-	printf(" --- --- --- --- --- --- --- --- \n\n");
+	print_chess(position, pieces, actual_pawn);
 
 	return (0);
 }
